6302660b42d4652e652b36e14dc163490adad192
Roy Bellingan <admin@seisho.us>
2018-11-09 21:58:04 +0000

    primi timidi passi

--- config.cpp
+++
@@ -1,4 +0,0 @@
-const QString  cartellaBase = "/home/cham/Downloads/jw/nwt_E/OEBPS/";
-const QString  estensione   = ".xhtml";
-const uint64_t inizioLibri  = 1001061105;
-const uint64_t numeroLibri  = 66;
--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include <QCoreApplication>
+//inclusine di header che provvedono funzionalita
+//qdebug serve a stampare a schermo
+//qfle serve ad operare sui i file
+//qstring serve ad effettuare delle operazioni sulle stringhe di testo
 #include <QDebug>
 #include <QFile>
 #include <QString>
-
+//config.cpp contiene la configurazione
 #include "config.cpp"
 
 //funzione che srotola i versetti in un capitolo
-void decodeChapter(QString capitolo) {
+void decodeChapter(QString& capitolo) {
 	int  inizioVersetto = 0;
 	bool continua       = true;
 	while (continua) {
@@ -28,13 +31,17 @@ void decodeChapter(QString capitolo) {
 }
 
 int main() {
+	//per scansionare i libri uno alla volta , ogni libro ha un suo id
 	for (uint64_t offsetLibro = 0; offsetLibro < numeroLibri; ++offsetLibro) {
+		//libro attuale e dato da dove iniziano i libri id5 a cui di volta in volta vado ad analizzare
 		auto    libroAttuale = inizioLibri + offsetLibro;
+		//stringa di testo
 		QString percorso     = cartellaBase + QString::number(libroAttuale) + estensione;
 		qDebug() << percorso;
 		uint64_t capitoloAttuale = 1;
 		for (;;) { //la condizione di arresto è complessa
 			//ora scansioniamo i capitoli 2...N
+			//i 2 piu vanno ad incrementare attuale+1
 			capitoloAttuale++;
 			QString percorsoCapitolo = cartellaBase + QString::number(libroAttuale) + "-split" + QString::number(capitoloAttuale) + estensione;
 			qDebug() << percorsoCapitolo;
@@ -45,7 +52,6 @@ int main() {
 				QString capitolo = file.readAll();
 				decodeChapter(capitolo);
 			}
-			exit(1);
 		}
 	}
 }
